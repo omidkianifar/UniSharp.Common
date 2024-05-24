0f280de80b19fdc9f49c9a0779a2b9e925d8947a
Kianifar.Omid <omid.kianifar@gmail.com>
2024-05-25 03:05:00 +0330

    Init

---
+++ Exceptions/Ensure.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace UniSharp.Common
+{
+    public static class Ensure
+    {
+        public static void ArgumentNotNull(object value, string paramName, string customMessage = null)
+        {
+            if (value is null)
+            {
+                throw new ArgumentNullException(paramName, customMessage);
+            }
+        }
+
+        public static void ReferenceNotNull(object value, string paramName)
+        {
+            if (value is null)
+            {
+                throw new NullReferenceException(paramName);
+            }
+        }
+
+        public static void ArgumentInRange(int value, int min, int max, string paramName)
+        {
+            if (value < min || value > max)
+            {
+                throw new ArgumentOutOfRangeException(paramName, $"Value should be between {min} and {max}.");
+            }
+        }
+
+        public static void ArgumentNotNegative(int value, string paramName)
+        {
+            if (value < 0)
+            {
+                throw new ArgumentOutOfRangeException(paramName, $"Value should be posetive.");
+            }
+        }
+
+        public static void ArgumentNotNegative(float value, string paramName)
+        {
+            if (value < 0)
+            {
+                throw new ArgumentOutOfRangeException(paramName, $"Value should be posetive.");
+            }
+        }
+
+        public static void ArgumentNotNegative(double value, string paramName)
+        {
+            if (value < 0)
+            {
+                throw new ArgumentOutOfRangeException(paramName, $"Value should be posetive.");
+            }
+        }
+
+        public static void ArgumentNotNegative(long value, string paramName)
+        {
+            if (value < 0)
+            {
+                throw new ArgumentOutOfRangeException(paramName, $"Value should be posetive.");
+            }
+        }
+
+        public static void ArgumentIsMatch(string param1Name, string param2Name, int param1Value, int param2Value)
+        {
+            if (param1Value != param2Value)
+            {
+                throw new ArgumentException($"{param1Name} not match with {param2Name}. values:{param1Value},{param2Value}.");
+            }
+        }
+
+        public static void KeyExists<TKey, TValue>(IDictionary<TKey, TValue> dictionary, TKey key, string dictionaryName, string keyName)
+        {
+            if (!dictionary.ContainsKey(key))
+            {
+                throw new KeyNotFoundException($"The key:{key} with name '{keyName}' was not found in the dictionary:{dictionaryName}.");
+            }
+        }
+
+        public static void CollectionNotEmpty<T>(ICollection<T> collection, string paramName)
+        {
+            if (collection == null || collection.Count == 0)
+            {
+                throw new ArgumentException($"The collection '{paramName}' cannot be null or empty.", paramName);
+            }
+        }
+
+        public static void IsValidEnumValue<TEnum>(TEnum value, string paramName) where TEnum : struct, Enum
+        {
+            if (!Enum.IsDefined(typeof(TEnum), value))
+            {
+                throw new ArgumentException($"The value '{value}' is not a valid enum value for type '{typeof(TEnum).Name}'.", paramName);
+            }
+        }
+
+        public static void IsOfType<TExpected>(object value, string paramName)
+        {
+            if (value is not TExpected)
+            {
+                throw new ArgumentException($"The parameter '{paramName}' is not of the expected type '{typeof(TExpected).Name}'.", paramName);
+            }
+        }
+    }
+}
---
+++ Results/Result.cs
@@ -0,0 +1,119 @@
+﻿using System;
+
+namespace UniSharp.Common
+{
+    public class Result
+    {
+        #region Properties
+
+        public bool IsSuccess { get; }
+
+        public string ErrorMessage { get; } = null;
+
+        public string StackTrace { get; } = null;
+
+        public string ErrorCode { get; set; } = null;
+
+        #endregion
+
+        #region Ctors
+
+        protected Result(bool isSuccess, string errorCode = null, string errorMessage = null, string stackTrace = null)
+        {
+            IsSuccess = isSuccess;
+            ErrorCode = errorCode;
+            ErrorMessage = errorMessage;
+            StackTrace = stackTrace;
+        }
+
+        #endregion
+
+        #region Public Methods
+
+        public static Result Success => new(true);
+
+        public static Result Fail(string errorCode, string errorMessage)
+        {
+            return new Result(false, errorCode, errorMessage);
+        }
+
+        public static implicit operator Result(Exception exception)
+        {
+            return new Result(false, "Exception", exception.Message, exception.StackTrace);
+        }
+
+        public override string ToString()
+        {
+            if (IsSuccess)
+                return "Success";
+
+            if (string.IsNullOrEmpty(StackTrace))
+                return $"Failure -> ErrorCode:{ErrorCode}, ErrorMessage:{ErrorMessage}";
+
+            return $"Failure -> ErrorCode:{ErrorCode}, ErrorMessage:{ErrorMessage}, StackTrace:{StackTrace}";
+        }
+
+        #endregion
+    }
+
+    public class Result<T> : Result
+    {
+        #region Properties
+
+        public T Value { get; }
+
+        #endregion
+
+        #region Ctors
+
+        private Result(T value) : base(true)
+        {
+            Value = value;
+        }
+
+        private Result(string errorCode = null, string errorMessage = null, string stackTrace = null) 
+            : base(false, errorCode, errorMessage, stackTrace)
+        {
+            Value = default;
+        }
+
+        #endregion
+
+        #region Public Methods
+
+        public static new Result<T> Success(T value) 
+        {
+            return new Result<T>(value);
+        }
+
+        public static Result<T> Fail(string errorCode, string errorMessage, string stackTrace = null)
+        {
+            return new Result<T>(errorCode, errorMessage, stackTrace);
+        }
+
+        public static Result<T> Fail(Result result)
+        {
+            return new Result<T>(result.ErrorCode, result.ErrorMessage, result.StackTrace);
+        }
+
+        public static implicit operator Result<T>(T value)
+        {
+            return new Result<T>(value);
+        }
+
+        public static implicit operator Result<T>(Exception exception)
+        {
+            return new Result<T>("Exception", exception.Message, exception.StackTrace);
+        }
+
+        public override string ToString()
+        {
+            if (IsSuccess)
+                return $"Success: {Value}";
+
+            return base.ToString();
+        }
+
+        #endregion
+    }
+}
